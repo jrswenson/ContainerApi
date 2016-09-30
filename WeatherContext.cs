dcfdc9dd7fe0aeef5f57532868b67b759ac38515
Jeff Swenson <jeffongit@gmail.com>
2016-09-29 18:13:16 -0600

    Adding Migration Stuff

--- WeatherContext.cs
+++ WeatherContext.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace ContainerApi
 {
     public class WeatherContext : DbContext{
+        public WeatherContext(DbContextOptions<WeatherContext> options): base(options) { }
+
+        public WeatherContext(){}
+
         public DbSet<WeatherEvent> WeatherEvents {get;set;}
         public DbSet<Reaction> Reactions {get;set;}
         public DbSet<Comment> Comments {get;set;}
